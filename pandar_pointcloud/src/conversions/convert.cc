05988e0bca0c1b07dd128ee7e320d222c9f553ce
张煜 <zhangyu@hesaitech.com>
2021-02-04 11:10:51 +0800

    change version

--- pandar_pointcloud/src/conversions/convert.cc
+++ pandar_pointcloud/src/conversions/convert.cc
@@ -83,8 +83,8 @@ Convert::Convert(ros::NodeHandle node, ros::NodeHandle private_nh,
     : data_(new pandar_rawdata::RawData()),
       drv(node, private_nh, node_type, this) {
   
-  m_sRosVersion = "Pandar128_1.0.7";
-  ROS_WARN("--------Pandar128 ROS version: %s--------\n\n",m_sRosVersion.c_str());
+  m_sRosVersion = "PandarSwift_1.0.8";
+  ROS_WARN("--------PandarSwift ROS version: %s--------\n\n",m_sRosVersion.c_str());
 
   publishmodel = "";
   if (LIDAR_NODE_TYPE == node_type) {
